85a377a9ecc566f7bc95610fc84a7dab14457d17
ariferdogannn <156123250+ariferdogannn@users.noreply.github.com>
2026-01-15 09:36:49 +0300

    added

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from send_payroll import send_payroll_mails
 
+
 app = Flask(__name__)
 app.secret_key = "bordro-secret"
+
 
 @app.route("/")
 def dashboard():
@@ -11,11 +13,13 @@ def dashboard():
         version="v1.0.3"
     )
 
+
 @app.route("/send", methods=["POST"])
 def send():
     report = send_payroll_mails()
     flash(report)
     return redirect(url_for("dashboard"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
